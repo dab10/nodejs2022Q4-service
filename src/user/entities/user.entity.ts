caeed9e4f794c10f6a78a82f6235852cf9495013
dab10 <dab10@list.ru>
2023-02-03 02:18:04 +0300

    feat: add get all users and create user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+export class User {
+  id: string;
+  login: string;
+  password: string;
+  version: number;
+  createdAt: number;
+  updatedAt: number;
+
+  constructor(id: string, login: string, password: string, version: number, createdAt: number, updatedAt: number) {
+    this.id = id;
+    this.login = login;
+    this.password = password;
+    this.version = version;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ParseUUIDPipe } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService {
   constructor(private db: DbService) {}
 
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = new User (
+      uuidv4(),
+      createUserDto.login,
+      createUserDto.password,
+      1,
+      Date.now(),
+      Date.now()
+    )
+    this.db.users.push(newUser);
+    return newUser;
   }
 
   findAll() {
